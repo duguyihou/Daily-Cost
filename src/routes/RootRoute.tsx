467834234af1bf99177e8c70093cbcdc141086c6
kong <duguyihou@gmail.com>
2023-01-02 08:36:39 +1100

    build: setup testing-library/react-native

--- src/routes/RootRoute.tsx
+++ src/routes/RootRoute.tsx
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Home from '@screens/Home'
+import { Home } from '@screens/home'
 import React from 'react'
 
 import { HomeStackParamList, RouteName } from './RootRoute.types'
