0b23a0cd0c8c18c0575d28f3f6ab5fb099af61fd
kong <duguyihou@gmail.com>
2023-01-01 14:33:17 +1100

    test: app

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,9 +1,3 @@
-/**
- * @format
- */
-
-import 'react-native'
-
 import React from 'react'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
--- jest.confg.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  transformIgnorePatterns: [
-    'node_modules/(?!@react-native|react-native)'
-  ],
-};
