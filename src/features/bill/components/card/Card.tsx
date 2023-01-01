bb1c6f1481b1534a645db12b96dbe3fe95b8e97f
kong <duguyihou@gmail.com>
2023-01-02 09:05:37 +1100

    feat(card): wip

--- src/features/bill/components/card/Card.tsx
+++ src/features/bill/components/card/Card.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 function Card() {
   return (
-    <View>
-      <Text>Card</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Coles</Text>
+      <Text style={styles.value}>$29.90</Text>
     </View>
   )
 }
 export default Card
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 16,
+  },
+  value: {
+    fontSize: 16,
+  },
+})
--- src/screens/home/__tests__/Home.tests.tsx
+++ src/screens/home/__tests__/Home.tests.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 
 import Home from '../Home'
 
+jest.mock('@features/bill', () => ({
+  Card: jest.fn(),
+}))
 test('home', () => {
   const home = render(<Home />)
   expect(home).toMatchSnapshot()
