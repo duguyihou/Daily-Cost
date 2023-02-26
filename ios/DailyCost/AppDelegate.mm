6bf380883d71d64bfe6b58334240077dae716fcb
kong <duguyihou@gmail.com>
2023-02-27 07:13:32 +1100

    feat: upgrade

--- ios/DailyCost/AppDelegate.mm
+++ ios/DailyCost/AppDelegate.mm
@@ -1,17 +1,16 @@
 #import "AppDelegate.h"
 
-
 #import <React/RCTBundleURLProvider.h>
-
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"DailyCost";
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
